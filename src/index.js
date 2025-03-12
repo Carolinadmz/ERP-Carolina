86ff4ca664f54a9a99df04a210f07ecfc4790b96
Carolina <carolina.dominguez.pe@usb.edu.mx>
2025-03-11 21:47:37 -0600

    Corregido el problema de pantalla en blanco después del login

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { Auth0Provider } from "@auth0/auth0-react"
+import { BrowserRouter } from "react-router-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <Auth0Provider
-    domain="dev-kdqtkxvgayafze2r.us.auth0.com"
-    clientId="CorEjmj7b1veNMtXYUGX4oSpDj9LFpb5"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
+      domain="dev-kdqtkxvgayafze2r.us.auth0.com"
+      clientId="CorEjmj7b1veNMtXYUGX4oSpDj9LFpb5"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
     >
       <BrowserRouter>
         <App />
       </BrowserRouter>
-  </Auth0Provider>
+    </Auth0Provider>
+  </React.StrictMode>,
+)
 
-  </React.StrictMode>
-);
-
-
-reportWebVitals();
+reportWebVitals()
